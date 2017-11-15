78fb3e2313c95f1627a422c895bdc9d92a2636a7
Lev Ostanin <lev.ostanin@gmail.com>
2017-11-16 02:48:23 +0300

    buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
-const App = () => (
-  <div>Test</div>
-)
+class App extends React.Component {
+  send = () => {
+    axios.post('http://api.cloudstitch.com/levkus/wedding', {
+      Name: 'LALALA',
+      Subject: 'Olalal',
+      Message: 'LolOll'
+    }).then(resp => console.log(resp))
+  }
+
+  get = () => {
+    axios.get('http://api.cloudstitch.com/levkus/wedding').then(resp => console.log(resp))
+  }
+
+  render () {
+    return(
+      <div>
+        <button onClick={this.send}>Send</button>
+        <button onClick={this.get}>Get</button>
+      </div>
+    )
+  }
+}
 
 export default App
